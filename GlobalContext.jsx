ec243eb285b0869c10014f67c403590f8be31cb2
Nevin Harold <cabarrubias.nevinharold@gmail.com>
2025-05-03 21:06:36 +0800

    Setting up Tailwind.css

---
+++ GlobalContext.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const GlobalContext = () => {
+  return (
+    <div>GlobalContext</div>
+  )
+}
+
+export default GlobalContext
+
+
+
+// KANI, mo ni if gusto nato na i-global an ID. For example, usa ka submittan, ma access sa tanan na dayun. As long as ma import sya sa main.jsx.
